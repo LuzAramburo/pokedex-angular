82eedc46067e10fb8739442e0550c372d414a34a
Luz Virginia Aramburo Alvarez <luzaramburo13@gmail.com>
2024-06-24 16:59:49 -0700

    get list

--- src/app/pokedex-list/pokedex-list.component.ts
+++ src/app/pokedex-list/pokedex-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Pokemon, PokemonService} from "../services/pokemon.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-pokedex-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './pokedex-list.component.html',
   styleUrl: './pokedex-list.component.css'
 })
-export class PokedexListComponent {
+export class PokedexListComponent implements OnInit {
+  private pokemonService = inject(PokemonService)
 
+  pokemons: Pokemon[] = []
+  isFetching = true
+  error: null | string = null
+
+  ngOnInit() {
+    this.pokemonService.getPokemons().subscribe({
+      next: (response) => {
+        this.pokemons = response.results
+      }
+    })
+  }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {PokemonResponse} from "../models/pokedex.model";
+
+const baseUrl = "https://pokeapi.co/api/v2/pokemon"
+const offset = 100
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  private http = inject(HttpClient)
 
-  constructor() { }
+  getPokemons () {
+    return this.http.get<PokemonResponse>(`${baseUrl}?limit=${offset}`)
+  }
 }
