63f15009523b5b82a928fb1b676fedca7ca6590a
Luz Virginia Aramburo Alvarez <luzaramburo13@gmail.com>
2024-06-24 16:25:21 -0700

    routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {PokedexListComponent} from "./pokedex-list/pokedex-list.component";
+import {PokemonDetailsComponent} from "./pokemon-details/pokemon-details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pokedex',
+    component: PokedexListComponent
+  },
+  {
+    path: 'pokedex/:pokemonId',
+    component: PokemonDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'pokedex',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/pokedex-list/pokedex-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokedex-list.component.html',
+  styleUrl: './pokedex-list.component.css'
+})
+export class PokedexListComponent {
+
+}
